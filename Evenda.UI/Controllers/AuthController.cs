7718bec783148bbde72daa2a6acd0be7a9dff274
Abdelrahman  Fawzy <abdelrahmanfawzy003@gmail.com>
2025-05-05 22:39:19 +0300

    create UI for login and register

---
+++ Evenda.UI/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Evenda.UI.Controllers
+{
+    [Route("auth")]
+    public class AuthController : Controller
+    {
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet("register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
