bc49a4a87191b370725b9ab0f0d4a484ccd44275
Abdelrahman  Fawzy <abdelrahmanfawzy003@gmail.com>
2025-05-10 18:33:01 +0300

    UI: adjustments for the navigation, footer and pagination helpers

---
+++ Evenda.UI/Helpers/Tags/DynamicAnchorTagHelper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Evenda.UI.TagHelpers
+{
+    [HtmlTargetElement("a", Attributes = "controller,action,route-params,page")]
+    public class DynamicAnchorTagHelper : AnchorTagHelper
+    {
+        public DynamicAnchorTagHelper(IHtmlGenerator generator) : base(generator)
+        {
+        }
+
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public int Page { get; set; }
+        public IDictionary<string, string?> RouteParams { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            RouteValues["controller"] = Controller;
+            RouteValues["action"] = Action;
+            RouteValues["pg"] = Page.ToString();
+
+            if (RouteParams != null)
+            {
+                foreach (var param in RouteParams)
+                {
+                    if (!string.IsNullOrEmpty(param.Value))
+                    {
+                        RouteValues[param.Key] = param.Value;
+                    }
+                }
+            }
+
+            base.Process(context, output);
+        }
+    }
+}
---
+++ Evenda.UI/Models/PaginationVM.cs
@@ -0,0 +1,11 @@
+﻿namespace Evenda.UI.Models
+{
+    public class PaginationVM
+    {
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public IDictionary<string, string?> RouteParams { get; set; } = new Dictionary<string, string?>();
+    }
+}
